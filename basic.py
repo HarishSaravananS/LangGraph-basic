7670408f457738d3fdb5e4750ae256e66c3c478c
HARISH S <119610526+HarishSaravananS@users.noreply.github.com>
2025-03-12 23:14:26 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,87 @@
+%%capture --no-stderr
+%pip install --quiet -U langchain_openai langchain_core langgraph
+import os
+import getpass
+
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+
+
+_set_env("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "langchain-academy"
+from langchain_groq import ChatGroq
+llm= ChatGroq(groq_api_key="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", model="llama-3.3-70b-versatile")
+from langchain_openai import ChatOpenAI
+
+def multiply(a: int, b: int) -> int:
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+# This will be a tool
+def add(a: int, b: int) -> int:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+def divide(a: int, b: int) -> float:
+    """Divide a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a / b
+
+tools = [add, multiply, divide]
+llm= ChatGroq(groq_api_key="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", model="llama-3.3-70b-versatile")
+
+# For this ipynb we set parallel tool calling to false as math generally is done sequentially, and this time we have 3 tools that can do math
+# the OpenAI model specifically defaults to parallel tool calling for efficiency, see https://python.langchain.com/docs/how_to/tool_calling_parallel/
+# play around with it and see how the model behaves with math equations!
+llm_with_tools = llm.bind_tools(tools, parallel_tool_calls=False)
+from langgraph.graph import MessagesState
+from langchain_core.messages import HumanMessage, SystemMessage
+
+# System message
+sys_msg = SystemMessage(content="You are a helpful assistant tasked with performing arithmetic on a set of inputs.")
+
+# Node
+def assistant(state: MessagesState):
+   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import tools_condition
+from langgraph.prebuilt import ToolNode
+from IPython.display import Image, display
+
+# Graph
+builder = StateGraph(MessagesState)
+
+# Define nodes: these do the work
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+
+# Define edges: these determine how the control flow moves
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+react_graph = builder.compile()
+
+# Show
+display(Image(react_graph.get_graph(xray=True).draw_mermaid_png()))
